f0e0138c10819a38c42611c5720eea86c1fdb6ff
Kelvin Li <kelli@ucdavis.edu>
2017-06-17 12:21:02 -0700

    Added Dropdown menu.

--- src/button-group.js
+++ src/button-group.js
@@ -34,5 +34,35 @@ function ButtonGroup(arg) {
     }
     buttonGroup.append = buttonGroup.add;
 
+    if(types.indexOf('dropdown')!==-1) {
+        var dropdown = document.createElement('div'),
+            icon = document.createElement('div'),
+            menu = document.createElement('div');
+
+        dropdown.className = 'ui floating dropdown icon button';
+        icon.className = 'dropdown icon';
+        menu.className = 'menu';
+
+        dropdown.appendChild(icon);
+        dropdown.appendChild(menu);
+        buttonGroup.appendChild(dropdown);
+
+        buttonGroup.menu.append = function(items) {
+            items.forEach(function(item){
+                var label = item.label || item.name || '',
+                    callback = item.onclick || function(){};
+                var itemDiv = document.createElement('div'),
+                    ic = document.createElement('i'),
+                    text = document.createTextNode(label);
+                ic.className = item.icon + ' icon';
+                itemDiv.appendChild(ic);
+                itemDiv.appendChild(text);
+                itemDiv.onclick = callback.call(this, arg);
+                itemDiv.className = 'item';
+                menu.appendChild(itemDiv);
+            })
+        }
+    }
+
     return buttonGroup;
 }
